d8b50b2a25fbae33336752e006bb1e4a3cc0ec0a
Henry Ifebunandu <henryifebunandu@gmail.com>
2023-06-24 09:29:07 +0100

    update: screen routes, ext on context

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -1,3 +1,6 @@
+import 'package:contacts_app_isidore/app/feature/home/ui/screen/home.dart';
+import 'package:contacts_app_isidore/app/feature/sign_in/ui/screen/sign_in.dart';
+import 'package:contacts_app_isidore/app/feature/sign_up/ui/screen/sign_up.dart';
 import 'package:contacts_app_isidore/core/router/route_names.dart';
 import 'package:flutter/material.dart';
 
@@ -5,15 +8,13 @@ class Router {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case '/':
-        return MaterialPageRoute(builder: (_) => const Placeholder());
-      case RouteNames.signIn:
-        return MaterialPageRoute(builder: (_) => const Placeholder());
+        return MaterialPageRoute(builder: (_) => const SignInScreen());
       case RouteNames.signUp:
-        return MaterialPageRoute(builder: (_) => const Placeholder());
+        return MaterialPageRoute(builder: (_) => const SignUpScreen());
       case RouteNames.home:
-        return MaterialPageRoute(builder: (_) => const Placeholder());
+        return MaterialPageRoute(builder: (_) => const HomeScreen());
       default:
-        return MaterialPageRoute(builder: (_) => const Placeholder());
+        return MaterialPageRoute(builder: (_) => const SignInScreen());
     }
   }
 }
